7bf1e2c0551d88224bc2682062dc779e6b340e14
Tiago Santos <taas@ua.pt>
2023-05-03 13:51:11 +0100

    Parte dos rolamentos feita mas a precisar de ser alterado para o ultimo veio

--- PME_5_ano/gear_shafts.m
+++ PME_5_ano/gear_shafts.m
@@ -47,22 +47,38 @@ P0_axial_a(idx) = 2.3*Fr_rolamento_a(idx)*tan(alpha_contacto_rol(idx))...
 P0_axial_b(idx) = 2.3*Fr_rolamento_b(idx)*tan(alpha_contacto_rol(idx))...
     + Fx(idx);
 
-if razao_forcas_rol_a > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_a(idx) > ceof_carga_axial(idx)
+    X_a(idx) = 0.41;
+    Y_a(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_a(idx) = 1.0;
+    Y_a(idx) = 0;
 end
 
-if razao_forcas_rol_b > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_b(idx) > ceof_carga_axial(idx)
+    X_b(idx) = 0.41;
+    Y_b(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_b(idx) = 1.0;
+    Y_b(idx) = 0;
 end
 
+fs(idx) = 1.2; % coefeciente de segurança - normal
+ft(idx) = 1.0; % fator de temperatura - <120º
+fl(idx) = 1.8; % fator de esforço dinâmico - motores elétricos pequenos
+fn(idx) = 0.281; % fator de rotação - 1500 rpm
+
+P0_a(idx) = X0(idx)*Fr_rolamento_a(idx) + Y0(idx)*Fx(idx);
+C0_a(idx) = fs(idx)*P0_a(idx);
+
+P0_b(idx) = X0(idx)*Fr_rolamento_b(idx) + Y0(idx)*Fx(idx);
+C0_b(idx) = fs(idx)*P0_b(idx);
+
+P_a(idx) = X_a(idx)*Fr_rolamento_a(idx) + Y_a(idx)*Fx(idx);
+C_a(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_a(idx);
+
+P_b(idx) = X_b(idx)*Fr_rolamento_b(idx) + Y_b(idx)*Fx(idx);
+C_b(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_b(idx);
 
 % Forças de engrenamento para o par Z3 e Z4 em N
 
@@ -102,21 +118,38 @@ P0_axial_a(idx) = 2.3*Fr_rolamento_a(idx)*tan(alpha_contacto_rol(idx))...
 P0_axial_b(idx) = 2.3*Fr_rolamento_b(idx)*tan(alpha_contacto_rol(idx))...
     + Fx(idx)-Fx(idx-1);
 
-if razao_forcas_rol_a > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_a(idx) > ceof_carga_axial(idx)
+    X_a(idx) = 0.41;
+    Y_a(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_a(idx) = 1.0;
+    Y_a(idx) = 0;
 end
 
-if razao_forcas_rol_b > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_b(idx) > ceof_carga_axial(idx)
+    X_b(idx) = 0.41;
+    Y_b(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_b(idx) = 1.0;
+    Y_b(idx) = 0;
 end
+
+fs(idx) = 1.2; % coefeciente de segurança - normal
+ft(idx) = 1.0; % fator de temperatura - <120º
+fl(idx) = 1.8; % fator de esforço dinâmico - motores elétricos pequenos
+fn(idx) = 0.281; % fator de rotação - 1500 rpm
+
+P0_a(idx) = X0(idx)*Fr_rolamento_a(idx) + Y0(idx)*(Fx(idx)-Fx(idx-1));
+C0_a(idx) = fs(idx)*P0_a(idx);
+
+P0_b(idx) = X0(idx)*Fr_rolamento_b(idx) + Y0(idx)*(Fx(idx)-Fx(idx-1));
+C0_b(idx) = fs(idx)*P0_b(idx);
+
+P_a(idx) = X_a(idx)*Fr_rolamento_a(idx) + Y_a(idx)*(Fx(idx)-Fx(idx-1));
+C_a(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_a(idx);
+
+P_b(idx) = X_b(idx)*Fr_rolamento_b(idx) + Y_b(idx)*(Fx(idx)-Fx(idx-1));
+C_b(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_b(idx);
 
 % Forças de engrenamento para o par Z5 e Z6 em N
 
@@ -155,21 +188,38 @@ P0_axial_a(idx) = 2.3*Fr_rolamento_a(idx)*tan(alpha_contacto_rol(idx))...
 P0_axial_b(idx) = 2.3*Fr_rolamento_b(idx)*tan(alpha_contacto_rol(idx))...
     + Fx(idx)-Fx(idx-1);
 
-if razao_forcas_rol_a > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_a(idx) > ceof_carga_axial(idx)
+    X_a(idx) = 0.41;
+    Y_a(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_a(idx) = 1.0;
+    Y_a(idx) = 0;
 end
 
-if razao_forcas_rol_b > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_b(idx) > ceof_carga_axial(idx)
+    X_b(idx) = 0.41;
+    Y_b(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_b(idx) = 1.0;
+    Y_b(idx) = 0;
 end
+
+fs(idx) = 1.2; % coefeciente de segurança - normal
+ft(idx) = 1.0; % fator de temperatura - <120º
+fl(idx) = 1.8; % fator de esforço dinâmico - motores elétricos pequenos
+fn(idx) = 0.281; % fator de rotação - 1500 rpm
+
+P0_a(idx) = X0(idx)*Fr_rolamento_a(idx) + Y0(idx)*(Fx(idx)-Fx(idx-1));
+C0_a(idx) = fs(idx)*P0_a(idx);
+
+P0_b(idx) = X0(idx)*Fr_rolamento_b(idx) + Y0(idx)*(Fx(idx)-Fx(idx-1));
+C0_b(idx) = fs(idx)*P0_b(idx);
+
+P_a(idx) = X_a(idx)*Fr_rolamento_a(idx) + Y_a(idx)*(Fx(idx)-Fx(idx-1));
+C_a(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_a(idx);
+
+P_b(idx) = X_b(idx)*Fr_rolamento_b(idx) + Y_b(idx)*(Fx(idx)-Fx(idx-1));
+C_b(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_b(idx);
 
 
 %Ultimo veio
@@ -191,27 +241,45 @@ Fr_rolamento_b(idx) = sqrt((R_b_xy(idx)^2)+R_b_zy(idx)^2);
 alpha_contacto_rol(idx) = deg2rad(25);
 X0(idx) = 0.5;
 Y0(idx) = 0.42;
-razao_forcas_rol_a(idx) = Fx(idx)/Fr_rolamento_a(idx);
-razao_forcas_rol_b(idx) = Fx(idx)/Fr_rolamento_b(idx);
+razao_forcas_rol_a(idx) = Fx(idx-1)/Fr_rolamento_a(idx);
+razao_forcas_rol_b(idx) = Fx(idx-1)/Fr_rolamento_b(idx);
 ceof_carga_axial(idx) = 0.68;
 P0_axial_a(idx) = 2.3*Fr_rolamento_a(idx)*tan(alpha_contacto_rol(idx))...
     + Fx(idx-1);
 P0_axial_b(idx) = 2.3*Fr_rolamento_b(idx)*tan(alpha_contacto_rol(idx))...
     + Fx(idx-1);
 
-if razao_forcas_rol_a > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_a(idx) > ceof_carga_axial(idx)
+    X_a(idx) = 0.41;
+    Y_a(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_a(idx) = 1.0;
+    Y_a(idx) = 0;
 end
 
-if razao_forcas_rol_b > ceof_carga_axial
-    X(idx) = 0.41;
-    Y(idx) = 0.87;
+if razao_forcas_rol_b(idx) > ceof_carga_axial(idx)
+    X_b(idx) = 0.41;
+    Y_b(idx) = 0.87;
 else
-    X(idx) = 1.0;
-    Y(idx) = 0;
+    X_b(idx) = 1.0;
+    Y_b(idx) = 0;
 end
+
+fs(idx) = 1.2; % coefeciente de segurança - normal
+ft(idx) = 1.0; % fator de temperatura - <120º
+fl(idx) = 1.8; % fator de esforço dinâmico - motores elétricos pequenos
+fn(idx) = 0.281; % fator de rotação - 1500 rpm
+
+P0_a(idx) = X0(idx)*Fr_rolamento_a(idx) + Y0(idx)*Fx(idx-1);
+C0_a(idx) = fs(idx)*P0_a(idx);
+
+P0_b(idx) = X0(idx)*Fr_rolamento_b(idx) + Y0(idx)*Fx(idx-1);
+C0_b(idx) = fs(idx)*P0_b(idx);
+
+P_a(idx) = X_a(idx)*Fr_rolamento_a(idx) + Y_a(idx)*Fx(idx-1);
+C_a(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_a(idx);
+
+P_b(idx) = X_b(idx)*Fr_rolamento_b(idx) + Y_b(idx)*Fx(idx-1);
+C_b(idx) = (fl(idx)/(fn(idx)*ft(idx)))*P_b(idx);
+
 
