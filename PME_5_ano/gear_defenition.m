9ba1da268f8fc7b8030d5dc01033c30fac5c41cb
Tiago Santos <taas@ua.pt>
2023-05-25 12:16:38 +0100

    Valores atualizados das rodas para 900 rpm

--- PME_5_ano/gear_defenition.m
+++ PME_5_ano/gear_defenition.m
@@ -34,21 +34,21 @@ U_total = w_entrada/w_saida;
 % Relação de transmição
 
 u(1) = 4;
-u(2) = 4;
-u(3) = 2.5;
+u(2) = 3.5;
+u(3) = 1.96;
 
 U_sizing_relation = u(1)*u(2)*u(3);
 
 %% Defenição de constantes para o cálculo do pinhão e das rodas
 
-% 20 anos, 300 dias, 8h, 60min, w de cada andar rpm
+% 20 anos, 250 dias, 4h, 60min, w de cada andar rpm
 Nciclos(1) = 20*250*4*60*w_entrada;
 Nciclos(2) = 20*250*4*60*w_entrada/u(1);
-Nciclos(3) = 20*250*4*60*w_entrada/u(2);
+Nciclos(3) = 20*250*4*60*(w_entrada/u(1))/u(2);
 
 beta(1) = deg2rad(20); % graus para radianos 
-beta(2) = deg2rad(35);
-beta(3) = deg2rad(40);
+beta(2) = deg2rad(20);
+beta(3) = deg2rad(30);
 alpha = deg2rad(20); % graus para radianos
 alpha_t(1) = atan(tan(alpha)/cos(beta(1))); % radianos
 alpha_t(2) = atan(tan(alpha)/cos(beta(2)));
@@ -57,7 +57,7 @@ KM = 1; % uniforme / uniforme
 Kbl(1) = log10(Nciclos(1))/8;
 Kbl(2) = log10(Nciclos(2))/8;
 Kbl(3) = log10(Nciclos(3))/8;
-Ye(1) = 1/1.5222; 
+Ye(1) = 0.8; 
 Ye(2) = 0.8;
 Ye(3) = 0.8;
 CL = 10; % Redutores P<=10kW; Mecânica Geral;
@@ -72,12 +72,12 @@ Ybeta(3) = 1/cos(beta(3));
 Khl(1) = 8/log10(Nciclos(1));
 Khl(2) = 8/log10(Nciclos(2));
 Khl(3) = 8/log10(Nciclos(3));
-oblim(1) = 240; % MPa - aço ao carbono C45
-oblim(2) = 285; % MPa - aço ao carbono temperado (óleo) 33Mn5
-oblim(3) = 285; % MPa - aço ao carbono temperado (óleo) 33Mn5
-ohlim(1) = 500; % MPa - aço ao carbono C45
-ohlim(2) = 745; % MPa - aço ao carbono temperado (óleo) 33Mn5
-ohlim(3) = 745; % MPa - aço ao carbono temperado (óleo) 33Mn5
+oblim(1) = 285; % MPa - aço ao carbono C45
+oblim(2) = 340; % MPa - aço ao carbono temperado (óleo) 33Mn5
+oblim(3) = 410; % MPa - aço ao carbono temperado (óleo) 33Mn5 MUDAR
+ohlim(1) = 745; % MPa - aço ao carbono C45
+ohlim(2) = 825; % MPa - aço ao carbono temperado (óleo) 33Mn5
+ohlim(3) = 980; % MPa - aço ao carbono temperado (óleo) 33Mn5 MUDAR
 E(1) = 200000;%MPa
 E(2) = 210000;%MPa
 E(3) = 210000;%MPa
@@ -89,7 +89,7 @@ idx = 1; % primeira engrenagem
 
 % Defenição dos valores iniciais do pinhão para o cálculo do módulo
 
-pinhao(idx).dentes = 17;
+pinhao(idx).dentes = 19;
 pinhao(idx).rotacao = w_entrada; %rpm
 pinhao(idx).dentes_virtual = pinhao(idx).dentes/(cos(beta(idx))^3);
 
@@ -105,7 +105,7 @@ mn_hertz(idx) = ((60000*P_motor_catalogo*cos(beta(idx))*KM*2*E(idx)*(u(idx)+1)).
 if mn_fad(idx) > mn_hertz(idx)
     mn(idx) = 1;
 else
-    mn(idx) = 5;
+    mn(idx) = 4.5;
 end
 
 %calculo das variáveis dependentes do módulo para o pinhão e roda
@@ -124,7 +124,7 @@ pinhao(idx).diametro_primitivo = pinhao(idx).dentes*mt(idx);
 pinhao(idx).diametro_base = pinhao(idx).diametro_primitivo*cos(alpha_t(idx));
 pinhao(idx).diametro_pe_dente = pinhao(idx).diametro_primitivo-2*hf;
 pinhao(idx).diametro_externo = pinhao(idx).diametro_primitivo+2*mn(idx);
-pinhao(idx).largura = 20; % valores assumidos e a serem alterados nos cálculos da fadiga
+pinhao(idx).largura = 35; % valores assumidos e a serem alterados nos cálculos da fadiga
 pinhao(idx).largura_dentado = pinhao(idx).largura/cos(beta(idx));
 
 % Cálculo dos parametros da roda da primeira engrenagem 
@@ -135,7 +135,7 @@ roda(idx).diametro_primitivo = roda(idx).dentes*mt(idx);
 roda(idx).diametro_base = roda(idx).diametro_primitivo*cos(alpha_t(idx));
 roda(idx).diametro_pe_dente = roda(idx).diametro_primitivo-2*hf;
 roda(idx).diametro_externo = roda(idx).diametro_primitivo+2*mn(idx);
-roda(idx).largura = 20; % valores assumidos e a serem alterados nos cálculos da fadiga
+roda(idx).largura = 35; % valores assumidos e a serem alterados nos cálculos da fadiga
 roda(idx).largura_dentado = roda(idx).largura/cos(beta(idx));
 
 % Cálculo do entre eixo da primeira engrenagem
@@ -146,7 +146,7 @@ idx = 2; % Segunda engrenagem
 
 % Defenição dos valores iniciais do pinhão para o cálculo do módulo
 
-pinhao(idx).dentes = 19;
+pinhao(idx).dentes = 22;
 pinhao(idx).rotacao = pinhao(idx-1).rotacao/u(1); %rpm
 pinhao(idx).dentes_virtual = pinhao(idx).dentes/cos(beta(idx))^3;
 
@@ -163,7 +163,7 @@ mn_hertz(idx) = ((60000*P_motor_catalogo*cos(beta(idx))*KM*2*E(idx)*(u(idx)+1)).
 if mn_fad(idx) > mn_hertz(idx)
     mn(idx) = 1.0;
 else
-    mn(idx) = 4.5;
+    mn(idx) = 6;
 end
 
 %calculo das variáveis dependentes do módulo para o pinhão e roda
@@ -204,7 +204,7 @@ idx = 3; % terceira engrenagem
 
 % Defenição dos valores iniciais do pinhão para o cálculo do módulo
 
-pinhao(idx).dentes = 25;
+pinhao(idx).dentes =38;
 pinhao(idx).rotacao = pinhao(idx-1).rotacao/u(2); %rpm
 pinhao(idx).dentes_virtual = pinhao(idx).dentes/cos(beta(idx))^3;
 
@@ -240,7 +240,7 @@ pinhao(idx).diametro_primitivo = pinhao(idx).dentes*mt(idx);
 pinhao(idx).diametro_base = pinhao(idx).diametro_primitivo*cos(alpha_t(idx));
 pinhao(idx).diametro_pe_dente = pinhao(idx).diametro_primitivo-2*hf(idx);
 pinhao(idx).diametro_externo = pinhao(idx).diametro_primitivo+2*mn(idx);
-pinhao(idx).largura = 60; % valores assumidos e a serem alterados nos cálculos da fadiga
+pinhao(idx).largura = 35; % valores assumidos e a serem alterados nos cálculos da fadiga
 pinhao(idx).largura_dentado = pinhao(idx).largura/cos(beta(idx));
 
 % Cálculo dos parametros da roda da primeira engrenagem 
